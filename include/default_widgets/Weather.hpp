0751909375ca35dee99aabfb449f0f1f7c4d1b5b
ProggerX <x@proggers.ru>
2023-12-30 14:29:10 +0300

    Deleted unusable class

--- include/default_widgets/Weather.hpp
+++ include/default_widgets/Weather.hpp
@@ -4,7 +4,3 @@ class CompactWeather : public CommandWidget {
 public:
 	CompactWeather (int x, int y, int height, int width) : CommandWidget(x, y, height, width, "curl 'https://wttr.in/?format=1' -s") {};
 };
-class Weather : public CommandWidget {
-public:
-	Weather (int x, int y, int height, int width) : CommandWidget(x, y, height, width, "curl 'https://wttr.in/' -s") {};
-};
